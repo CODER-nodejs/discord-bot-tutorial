d6d4f8dd0a5a798e9dcb1d8c14425fd2a5c906a3
CODER-nodejs <77936905+CODER-nodejs@users.noreply.github.com>
2021-01-24 20:41:40 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ //You can edit the prefix here
+const prefix = '-';
+ 
+client.commands = new Discord.Collection();
+ 
+ //This prompts if the bot is online
+client.once('ready', () => {
+    console.log('Bot is online :)');
+});
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    } 
+});
+ //Paste your token here
+client.login('Your Bot token');
